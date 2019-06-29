a32047097dc13c319c48319db6f080860b86d801
ItsNamrathaB <namrathabhere@gmail.com>
2019-06-29 20:56:07 +0530

    Fetches data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ useState,useEffect} from 'react';
+const outerDivStyle = {
+  width:'100%',
+  height:'100vh',
+  backgroundColor:'#FAE7F9'
+};
+const tableWrapperStyle = {
+  color:'#781271',
+  whiteSpace: 'pre-line'
+};
+const tableStyle = {
+  width:'100%',
+  height:'99vh'
+};
+const tableHeaderStyle = {
+  padding: '15px',
+  paddingLeft:'0px',
+  textAlign:'left',
+  fontWeight:500
+};
+const headerRowStyle = {
+  backgroundColor:'#F0C5ED'
+};
+const tableRowStyle = {
+  backgroundColor:'#F7DCF5'
+};
 function App() {
+  const [hasErrors, setErrors] = useState(false);
+  const [people,setPeople] = useState({});
+
+  async function fetchData(){
+    const res= await fetch("https://swapi.co/api/people/1");
+    res.json().then(res => setPeople(res)).catch(err => setErrors(err));
+  }
+  useEffect(()=>{fetchData();});
+  function generateTable(){
+    let table = []
+    for (var key in people) {
+      let row = []
+      row.push(<td>{key}</td>)
+      row.push(<td>{people[key]}</td>)
+      table.push(<tr style={tableRowStyle}>{row}</tr>)
+    }
+    table.push(<tr><td>PS: I love purple :)</td></tr>);
+    return table
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={outerDivStyle}>
+      <div>
+        <span style={tableWrapperStyle}>
+          <table style={tableStyle}>
+            <tr style={headerRowStyle}>
+            <th style={tableHeaderStyle}>key</th>
+            <th style={tableHeaderStyle}>Value</th>
+            </tr>
+            {
+              generateTable()
+            }
+          </table>
+        </span>
+      </div>
     </div>
   );
 }
